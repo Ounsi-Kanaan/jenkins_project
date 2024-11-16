d26febe09d1fa5643e39e41afbb9441c178526fb
Ounsi-Kanaan <125888321+Ounsi-Kanaan@users.noreply.github.com>
2024-11-16 19:54:53 +0200

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -4,4 +4,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import greet
 
 def test_greet():
-    assert greet("World") == "Hello, World from FirstName LastName!"
+    assert greet("World") == "Hello, World from Ounsi Kanaan!"
